4b4519c3c16f32cb61b8b5d301eceebd0dc40b0e
WONDongin <88768560+WONDongin@users.noreply.github.com>
2024-02-25 23:50:30 +0900

    [JAVA-12] 중첩 if문
    중첩 if문

--- src/ch04/IfExample.java
+++ src/ch04/IfExample.java
@@ -2,12 +2,15 @@ package ch04;
 
 public class IfExample {
 	public static void main(String[] args) {
-		int score = 93;
+		int score = 92;
 		
 		if(score >=90) {
-			System.out.println("A 등급");
-		}if(score < 90) {
-			System.out.println("B등");
+			System.out.println("통과");
+		}else if(score >= 80) {
+			System.out.println("보류");
+		//80점 미만
+ 		}else {
+			System.out.println("탈락");
 		}
 	}
 
